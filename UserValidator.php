e82f23b5a5c823de2c4ec992d69a52434f1259e9
DeniceVO <daniel.hackerski@gmail.com>
2024-09-06 21:31:15 +0000

    main

--- UserValidator.php
+++ UserValidator.php
@@ -2,7 +2,7 @@
 
 class UserValidator
 {
-    private const EMAIL_PATTERN = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+    private const EMAIL_PATTERN = '/^[a-zA-Z]+[a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
     private const PASSWORD_MIN_LENGTH = 8;
     private const UPPERCASE_PATTERN = '/[A-Z]/';
     private const LOWERCASE_PATTERN = '/[a-z]/';
@@ -22,11 +22,13 @@ class UserValidator
             return false;
         }
 
+        // Sprawdzenie poszczególnych warunków
         $hasUppercase = preg_match(self::UPPERCASE_PATTERN, $password);
         $hasLowercase = preg_match(self::LOWERCASE_PATTERN, $password);
         $hasDigit = preg_match(self::DIGIT_PATTERN, $password);
         $hasSpecialChar = preg_match(self::SPECIAL_CHAR_PATTERN, $password);
 
+        // Zwracamy true tylko wtedy, gdy wszystkie warunki są spełnione
         return $hasUppercase && $hasLowercase && $hasDigit && $hasSpecialChar;
     }
 
@@ -37,19 +39,23 @@ class UserValidator
     }
 }
 
+// Przykład użycia:
 $validator = new UserValidator();
 
 $email = "test@example.com";
 $password = "StrongPass1!";
 
+// Walidacja e-mail
 if ($validator->validateEmail($email)) {
     echo "Email is valid.\n";
 } else {
     echo "Email is invalid.\n";
 }
 
+// Walidacja hasła
 if ($validator->validatePassword($password)) {
     echo "Password is valid.\n";
 } else {
     echo "Password is invalid.\n";
 }
+
